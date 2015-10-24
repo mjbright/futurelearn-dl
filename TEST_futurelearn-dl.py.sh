3379041a78d7ad3b401ddc0c7a695b7a93fdff6f
mjbright <github.com@mjbright.net>
2015-10-24 20:35:27 +0200

    Cleaned up code a minimum\!\!

---
+++ TEST_futurelearn-dl.py.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+./futurelearn-dl.py YOUR_LOGIN YOUR_PASSWORD COURSE_ID
+
+# e.g.
+#    ./futurelearn-dl.py joe joespass data-to-insight
+
+
--- futurelearn-dl.py
+++ futurelearn-dl.py
@@ -1,26 +1,60 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os, errno
 import requests
 import json
 
-session = requests.Session()
+'''
+    Author: Michael Bright, @mjbright
+    Created: 2015-Oct-24
+
+    Attempt at auto-downloading videos and other materials from futurelearn.com
+
+    TODO: Do proper argument handling and associated processing
+    - specify e-mail, password, course_id, week, type
+    - specify temp/output dirs, debug
+
+'''
 
 SIGNIN_URL = 'https://www.futurelearn.com/sign-in'
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
-    #'content-type': 'application/txt',
-    'content-type': 'application/json',
-}
+DEBUG=True
+
+TMP_DIR=os.getenv('HOME') + '/tmp/FUTURELEARN_DL'
+OP_DIR=os.getenv('HOME') + '/FUTURELEARN_DL'
 
 email = sys.argv[1]
 password = sys.argv[2]
 course_id = sys.argv[3]
 
-print("Using e-mail={} password=***** course_id={}".format(email, course_id))
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
+    'content-type': 'application/json',
+}
+
+## -- Functions: ---------------------------------------------------
+
+def fatal(msg):
+    ''' die brutally '''
+    print("FATAL:" + msg)
+    sys.exit(1)
+
+def mkdir_p(path):
+    '''
+         Perform equivalent of 'mkdir -p path' to create a directory and any necessary
+         parent directories
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
 
 def showResponse(response):
+    '''
+       Dump various fields of a http response
+    '''
     print("url=" + response.url)
     print("request=" + str(response.request))
     print("request.method=" + str(response.request.method))
@@ -28,21 +62,39 @@ def showResponse(response):
     print("RESPONSE cookies(response)=" + str(response.cookies))
     print("RESPONSE len(response.content)=" + str(len(response.content)))
     print("status_code=" + str(response.status_code))
-
-def fatal(msg):
-    print("FATAL:" + msg)
-    sys.exit(1)
+    print(str(response))
+    print("reason={}".format(str(response.reason)))
+    print("status_code={}".format(str(response.status_code)))
+    #print("content={}".format(str(response.content)))
+    print("encoding={}".format(str(response.encoding)))
+    print("headers={}".format(str(response.headers)))
+    print("history={}".format(str(response.history)))
+    print("is_redirect={}".format(str(response.is_redirect)))
+    print("ok={}".format(str(response.ok)))
+    print("links={}".format(str(response.links)))
+    print("raise_for_status={}".format(str(response.raise_for_status)))
+    print("raw={}".format(str(response.raw)))
+    print("text={}".format(str(response.text)))
+    print("url={}".format(str(response.url)))
+    print("json={}".format(str(response.json)))
 
 def writeFile(file, content):
+    ''' Write content to the specified file '''
     f = open(file, 'w')
     f.write(content)
     f.close()
 
-def getToken(url):
+def getToken(session, url):
+    ''' Perform request to the specified signin url and extract the 'authenticity_token'
+        RETURN: token and cookies
+    '''
     response = session.get(url, headers=headers)
     #showResponse(response)
     content = response.content.decode('utf8')
-    writeFile('token.response.content', content)
+    if DEBUG:
+        ofile= TMP_DIR + '/token.response.content'
+        print("Writing 'getToken' response to <{}>".format(ofile))
+        writeFile(ofile, content)
 
     apos = content.find("authenticity_token")
     if apos == -1:
@@ -62,44 +114,35 @@ def getToken(url):
     print("TOKEN='{}'".format(token))
     return token, response.cookies
 
-def login(url, email, password, token, cookies):
+def login(session, url, email, password, token, cookies):
+    ''' Perform request to the specified signin url to perform site login
+        RETURN: response
+    '''
     data=json.dumps({'email': email, 'password':password, 'authenticity_token':token})
-    print("COOKIES={}".format( str(cookies) ))
-    #data='''
-#email={}
-#password={}
-#authenticity_token={}
-#'''.format(email, password, token)
+    #print("COOKIES={}".format( str(cookies) ))
+
     response = session.post(url, headers=headers, cookies=cookies, data=data)
     content = response.content.decode('utf8')
-    writeFile('login.response.content', content)
+    if DEBUG:
+        ofile= TMP_DIR + '/login.response.content'
+        print("Writing 'login' response to <{}>".format(ofile))
+        writeFile(ofile, content)
+
     return response
 
-#--data-urlencode email=$email
-#--data-urlencode password=$password
-#--data-urlencode authenticity_token=$TOKEN $URL2"
+## -- Main: --------------------------------------------------------
 
-#do the login
-token, cookies = getToken(SIGNIN_URL)
-response = login(SIGNIN_URL, email, password, token, cookies)
+session = requests.Session()
 
-print(str(response))
-#print(str(dir(response)))
-#['__attrs__', '__bool__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__iter__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__nonzero__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_content', '_content_consumed', 'apparent_encoding', 'close', 'connection', 'content', 'cookies', 'elapsed', 'encoding', 'headers', 'history', 'is_permanent_redirect', 'is_redirect', 'iter_content', 'iter_lines', 'json', 'links', 'ok', 'raise_for_status', 'raw', 'reason', 'request', 'status_code', 'text', 'url']
-print("reason={}".format(str(response.reason)))
-print("status_code={}".format(str(response.status_code)))
-#print("content={}".format(str(response.content)))
-print("encoding={}".format(str(response.encoding)))
-print("headers={}".format(str(response.headers)))
-print("history={}".format(str(response.history)))
-print("is_redirect={}".format(str(response.is_redirect)))
-print("ok={}".format(str(response.ok)))
-print("links={}".format(str(response.links)))
-print("raise_for_status={}".format(str(response.raise_for_status)))
-print("raw={}".format(str(response.raw)))
-print("text={}".format(str(response.text)))
-print("url={}".format(str(response.url)))
-print("json={}".format(str(response.json)))
+mkdir_p(TMP_DIR)
+if not os.path.isdir(TMP_DIR):
+    fatal("Failed to create tmp dir <{}>".format(TMP_DIR))
+
+print("Using e-mail={} password=***** course_id={}".format(email, course_id))
+
+## -- do the login:
+token, cookies = getToken(session, SIGNIN_URL)
+response = login(session, SIGNIN_URL, email, password, token, cookies)
 
 sys.exit(0)
 ################################################################################
@@ -110,47 +153,5 @@ sys.exit(0)
 # RESPONSE methods:
 #['__attrs__', '__bool__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__iter__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__nonzero__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_content', '_content_consumed', 'apparent_encoding', 'close', 'connection', 'content', 'cookies', 'elapsed', 'encoding', 'headers', 'history', 'is_permanent_redirect', 'is_redirect', 'iter_content', 'iter_lines', 'json', 'links', 'ok', 'raise_for_status', 'raw', 'reason', 'request', 'status_code', 'text', 'url']
 
-
-#response = conn.getresponse()
-#print(response.status, response.reason)
-#print( dir(r) )
-#print("CONTENT=")
-#content = r.content
-#content = bytes(r.content, 'UTF-8')
-#content = r.content.decode('utf-8', errors='replace')
-
-
-#print(dir(response.content))
-#f.write(response.content.decode('utf8'))
-
-#content = response.content.encode('utf-8', errors='replace')
-#print(content)
-
-sys.exit(0)
-
-
-print("R=")
-print(r)
-token = r['response']['user']['authentication_token']    #set token value
-print("TOKEN=")
-print(token)
-response = r.json()
-token = response['response']['user']['authentication_token']    #set token value
-print(token)
-sys.exit(0)
-
-#r = session.post(SIGNIN_URL,
-                    #data=json.dumps({'email': email, 'password':password}), 
-                    #headers={'content-type': 'application/json'})
-#response = r.json()
-
-print(response) #check response
-token = response['response']['user']['authentication_token']    #set token value
-
-#Now you can do authorised calls
-#r = session.get('http://localhost:5000/protected', 
-r = session.get(SIGNIN_URL,
-                headers={'Authentication-Token': token})
-print(r.text)
 
 
